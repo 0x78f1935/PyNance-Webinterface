e005c044d5811b7637a6038a03503ccd48a4d377
0x78f1935 <codewars.nl@gmail.com>
2021-02-28 05:28:08 +0100

    Fixed some spelling mistakes; added links to github / github

--- frontend/src/components/GlobalFooter.vue
+++ frontend/src/components/GlobalFooter.vue
@@ -1,6 +1,16 @@
 <template>
     <v-footer app>
-        Version: {{$store.getters.version}}
+        <b-col>
+            Version: {{$store.getters.version}}
+        </b-col>
+        <b-col class="mr-auro to_the_right">
+            <v-btn icon>
+                <v-icon @click="redirect_twitter">mdi-twitter</v-icon>
+            </v-btn>
+            <v-btn icon>
+                <v-icon @click="redirect_github">mdi-github</v-icon>
+            </v-btn>
+        </b-col>
     </v-footer>
 </template>
 
@@ -12,11 +22,20 @@
         methods: {
             format_price(amount) {
                 return price(amount, this.$store.getters.metrics);
-            }
+            },
+            redirect_twitter() {
+                window.open(this.$store.getters.twitter, '_blank');
+            },
+            redirect_github() {
+                window.open(this.$store.getters.github, '_blank');
+            },
         },
     }
 </script>
 
 <style lang="scss" scoped>
-
+    .to_the_right {
+        flex-direction: column-reverse;
+        display: contents;
+    }
 </style>
