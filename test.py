322f5fd1a4794e2ff5b65c604c18fa6bd0bab4ba
0x78f1935 <codewars.nl@gmail.com>
2021-02-28 18:31:38 +0100

    Fixed listener in the webserver

--- test.py
+++
@@ -1,7 +0,0 @@
-import requests
-import time
-
-while True:
-    data = requests.get('http://127.0.0.1:5000/api/v1/system/')
-    print(data.json())
-    time.sleep(1)
--- webserver.py
+++ webserver.py
@@ -1,10 +1,12 @@
 from backend import Webserver
+from listener import Listener
 
 def start_le_server() -> object:
     """
     This method is used to host the backend server with gunicorn
     """
     server = Webserver()
+    Listener()
     return server
 
 server = Webserver()
