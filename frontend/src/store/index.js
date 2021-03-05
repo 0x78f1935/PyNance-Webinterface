eaa3db6d587bac4658e460a257f4420d6bd2dbc2
0x78f1935 <codewars.nl@gmail.com>
2021-03-05 04:15:47 +0100

    Fixed issue where state was not defined

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -60,78 +60,78 @@ export default new Vuex.Store({
     SET_PROFIT(state, value) { state.profit = value; },
   },
   actions: {
-    get_chatterer({ commit, getters }) {
-      axios.get(`${getters.endpoint}/api/v1/ui/knightrider`).then(response => {
-        commit('SET_CHATTERER', response.data.chatterer);
+    get_chatterer(state) {
+      axios.get(`${state.getters.endpoint}/api/v1/ui/knightrider`).then(response => {
+        state.commit('SET_CHATTERER', response.data.chatterer);
       })
     },
-    get_version({ commit, getters }) {
-      axios.get(`${getters.endpoint}/api/v1/ui/version`).then(response => {
-        commit('SET_VERSION', response.data.version);
+    get_version(state) {
+      axios.get(`${state.getters.endpoint}/api/v1/ui/version`).then(response => {
+        state.commit('SET_VERSION', response.data.version);
       })
     },
-    get_profit({ commit, getters }) {
-      axios.get(`${getters.endpoint}/api/v1/ui/profit`).then(response => {
-        commit('SET_PROFIT', response.data.profit);
+    get_profit(state) {
+      axios.get(`${state.getters.endpoint}/api/v1/ui/profit`).then(response => {
+        state.commit('SET_PROFIT', response.data.profit);
       })
     },
-    get_maintainer({ commit, getters }) {
-      axios.get(`${getters.endpoint}/api/v1/ui/maintainer`).then(response => {
-        commit('SET_MAINTAINER', response.data.maintainer);
-        commit('SET_GITHUB', response.data.github);
-        commit('SET_TWITTER', response.data.twitter);
+    get_maintainer(state) {
+      axios.get(`${state.getters.endpoint}/api/v1/ui/maintainer`).then(response => {
+        state.commit('SET_MAINTAINER', response.data.maintainer);
+        state.commit('SET_GITHUB', response.data.github);
+        state.commit('SET_TWITTER', response.data.twitter);
       })
     },
-    get_online({commit, getters}) {
-      axios.get(`${getters.endpoint}/api/v1/ui/online`).then(response => {
-        commit('SET_ONLINE', response.data.online);
+    get_online(state) {
+      axios.get(`${state.getters.endpoint}/api/v1/ui/online`).then(response => {
+        state.commit('SET_ONLINE', response.data.online);
       })
     },
-    get_currencies({commit, getters}) {
-      axios.get(`${getters.endpoint}/api/v1/ui/currency`).then(response => {
-        commit('SET_CUR1', response.data.cur1);
-        commit('SET_CUR2', response.data.cur2);
+    get_currencies(state) {
+      axios.get(`${state.getters.endpoint}/api/v1/ui/currency`).then(response => {
+        state.commit('SET_CUR1', response.data.cur1);
+        state.commit('SET_CUR2', response.data.cur2);
       })
     },
-    get_symbols({commit, getters}) {
-      axios.get(`${getters.endpoint}/api/v1/ui/symbols`).then(response => {
-        commit('SET_SYMBOLS', response.data.symbols);
+    get_symbols(state) {
+      axios.get(`${state.getters.endpoint}/api/v1/ui/symbols`).then(response => {
+        state.commit('SET_SYMBOLS', response.data.symbols);
       })
-      axios.get(`${getters.endpoint}/api/v1/ui/symbols_sets`).then(response => {
-        commit('SET_SYMBOLS_SETS', response.data.symbols);
+      axios.get(`${state.getters.endpoint}/api/v1/ui/symbols_sets`).then(response => {
+        state.commit('SET_SYMBOLS_SETS', response.data.symbols);
       })
     },
-    get_take_profit({commit, getters}) {
-      axios.get(`${getters.endpoint}/api/v1/configure/take_profit`).then(response => {
-        commit('SET_TAKE_PROFIT', response.data.take_profit);
+    get_take_profit(state) {
+      axios.get(`${state.getters.endpoint}/api/v1/configure/take_profit`).then(response => {
+        state.commit('SET_TAKE_PROFIT', response.data.take_profit);
       })
     },
     set_take_profit(state, value) {
       if(value != state.take_profit) {
-        axios.post(`${getters.endpoint}/api/v1/configure/take_profit`, {'tp': value});
+        axios.post(`${state.getters.endpoint}/api/v1/configure/take_profit`, {'tp': value});
       }
     },
-    toggle_online() {
-      axios.get(`${getters.endpoint}/api/v1/configure/online`);
+    toggle_online(state) {
+      axios.get(`${state.getters.endpoint}/api/v1/configure/online`);
     },
-    set_cur1({commit, getters}, value) {
-      axios.post(`${getters.endpoint}/api/v1/configure/cur1`, {'cur': value}).then(response => {
-        commit('SET_CUR1', response.data.cur1);
-        commit('SET_CUR2', response.data.cur2);
+    set_cur1(state, value) {
+      axios.post(`${state.getters.endpoint}/api/v1/configure/cur1`, {'cur': value}).then(response => {
+        state.commit('SET_CUR1', response.data.cur1);
+        state.commit('SET_CUR2', response.data.cur2);
       });
     },
-    set_cur2({commit, getters}, value) {
-      axios.post(`${getters.endpoint}/api/v1/configure/cur2`, {'cur': value}).then(response => {
-        commit('SET_CUR1', response.data.cur1);
-        commit('SET_CUR2', response.data.cur2);
+    set_cur2(state, value) {
+      axios.post(`${state.getters.endpoint}/api/v1/configure/cur2`, {'cur': value}).then(response => {
+        state.commit('SET_CUR1', response.data.cur1);
+        state.commit('SET_CUR2', response.data.cur2);
       });
     },
     toggle_drawer(state, v) {
-      state.commit('SET_DRAWER', v);
+      state.state.commit('SET_DRAWER', v);
     },
-    set_panik({commit, getters}, value) {
-      axios.post(`${getters.endpoint}/api/v1/configure/panik`, {'panik': value}).then(response => {
-        commit('SET_PANIK', response.data.panik);
+    set_panik(state, value) {
+      axios.post(`${state.getters.endpoint}/api/v1/configure/panik`, {'panik': value}).then(response => {
+        state.commit('SET_PANIK', response.data.panik);
       });
     },
   },
