0c80376ed29451e62f955de1b625e38b107f8f96
0x78f1935 <codewars.nl@gmail.com>
2021-03-21 18:06:20 +0100

    Cleaned repo to prep for latest version

--- backend/jinja.py
+++
@@ -1,8 +0,0 @@
-from flask import current_app
-from datetime import datetime
-
-
-class CustomFunctions(object):
-    @staticmethod
-    def project_name():
-        return current_app.config['PROJECT_NAME']
