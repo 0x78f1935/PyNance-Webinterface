b83759f68d6a27293c7cad733e614166dcce25ec
0x78f1935 <codewars.nl@gmail.com>
2021-05-02 07:59:54 +0200

    Added more logic!

--- backend/views/api/logic.py
+++ backend/views/api/logic.py
@@ -5,6 +5,7 @@ from backend.utils.auth import login_required
 from backend import db, pynance
 
 from datetime import datetime
+import math
 
 
 class LogicApiView(FlaskView):
@@ -33,6 +34,14 @@ class LogicApiView(FlaskView):
                 'online': False,
                 'execution_time': str(datetime.now()-stopwatch),
             })
+        
+        binance_status = pynance.system.maintenance()
+        if binance_status.json['status'] != 0:
+            bot.chat("BINANCE IS CURRENTLY UNAVAILABLE")
+            return jsonify({
+                'online': True,
+                'execution_time': str(datetime.now()-stopwatch),
+            })
 
         # Check if there is a configuration based on selected symbols
         if len(bot.config.symbols) <= 0:
@@ -44,12 +53,42 @@ class LogicApiView(FlaskView):
 
         # Iterate over each symbol
         for symbol in bot.config.symbols:
+            # Update the current average
             bot.chat(f"CURRENT SYMBOL SELECTED - {symbol}")
             average = pynance.assets.average(symbol, bot.config.timeframe, bot.config.candle_interval)
+            if bot.config.below_average > 0: average = float(average - float(float(average/100) * bot.config.below_average))
             bot.update_average(average)
 
             # Check if this symbol has any open orders
             order = bot.get_order(symbol)
+
+            # Check if we are buying or if we are selling
+            # One full cycle if buying asset B and selling asset B for asset A
+            if order.buying:
+                minimal_profit = bot.config.profit_margin
+                asset_fees = pynance.assets.fees(symbol)  # makerCommission | takerCommission
+                asset_exchange_info = pynance.assets.exchange_info(symbol).json['symbols'].pop(0)
+                # Calculate the precision size of the symbol
+                stepSize = [ i for i in asset_exchange_info['filters'] if i['filterType'] == 'LOT_SIZE'][0]['stepSize']
+                precision = int(round(-math.log(float(stepSize), 10), 0))
+                # Get the asset names
+                base_asset = asset_exchange_info['baseAsset']
+                quote_asset = asset_exchange_info['quoteAsset']
+
+                # Check for each asset selected what we have available in the wallet
+                balance = pynance.wallet.balance()
+                base_balance = [i for i in balance.json if i['coin'] == base_asset][0]
+                balance_free = float(round(float(base_balance['free']), 8))
+                if balance_free < 10:
+                    bot.chat(f"NOT ENOUGH {base_asset} TO BUY - {quote_asset} - {balance_free} {base_asset} AVAILABLE NEED 10 {base_asset}")
+                    return jsonify({
+                        'online': True,
+                        'execution_time': str(datetime.now()-stopwatch),
+                    })
+                
+
+                print(1)
+
         
         return jsonify({
             'online': True,
